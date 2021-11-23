6f35f00de94b103c47d09e65c5d55e74068b327d
Daniel Pich <danielpich@Daniels-MacBook-Pro.local>
2021-11-23 18:35:50 +0200

    try jenkins

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hiiii
+          Try jenkins
         </p>
         <a
           className="App-link"
